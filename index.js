22d58661868a96adf19dd2a76d0b4fbb94064af8
botirmasharipov <masbotirjon963@gmail.com>
2022-12-12 22:23:25 -0500

    create association

---
+++ index.js
@@ -0,0 +1,12 @@
+const { User } = require('./models/User')
+const { Board } = require('./models/Board')
+const { Cheese } = require('./models/Cheese')
+
+User.belongsTo(Board)
+Board.hasMany(Cheese)
+
+module.exports = {
+    User,
+    Board,
+    Cheese
+}
