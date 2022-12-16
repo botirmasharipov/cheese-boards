ca0696e60bc07340c57642b61b59f43dc361de56
botirmasharipov <masbotirjon963@gmail.com>
2022-12-16 16:30:56 -0500

    define one-to-many relationship between User and Board

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const { Board } = require('./models/Board')
 const { Cheese } = require('./models/Cheese')
 
 User.belongsTo(Board)
+User.hasMany(Board)
 
 Board.belongsToMany(Cheese, { through: 'cheese_boards' })
 Cheese.belongsToMany(Board, { through: 'cheese_boards' })
